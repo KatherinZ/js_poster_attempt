50eb993b9d99f310a0ef11f7ba1cff4ae4ce7f05
Katherin_Z <zyrianovakate10@gmail.com>
2020-02-28 22:11:43 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
 //   translateX: function() { return anime.random(50, 250); },
 // });
 
-let elementsArray = document.querySelectorAll('.all_elements .square_1')
-function shuffle(elementsArray){
-let count = 0
-while (elementsArray.length > count) {
-  square_1.style.translateX = getRandomValue()
-  square_1.style.translateY = getRandomValue()
-  count += 1
-}
-return elementsArray
-}
-onload shuffle(elementsArray)
+// let elementsArray = document.querySelectorAll('.all_elements .square_1')
+// function shuffle(elementsArray){
+// let count = 0
+// while (elementsArray.length > count) {
+//   square_1.style.translateX = getRandomValue()
+//   square_1.style.translateY = getRandomValue()
+//   count += 1
+// }
+// return elementsArray
+// }
+// onload shuffle(elementsArray)
+
+
+[...document.querySelectorAll('.square_1')].map(function(el) {
+    el.style.transform = `translate(${anime.random(0, 100)}vw, ${anime.random(0,100)}vh)`
+})
