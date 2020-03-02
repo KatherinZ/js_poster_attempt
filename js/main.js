6aaacda51df2ca8d2663f2fd51dc9248bdec6bc5
Katherin_Z <zyrianovakate10@gmail.com>
2020-03-03 00:14:44 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -14,7 +14,40 @@
 // }
 // onload shuffle(elementsArray)
 
-
-[...document.querySelectorAll('.square_1')].map(function(el) {
-    el.style.transform = `translate(${anime.random(0, 100)}vw, ${anime.random(0,100)}vh)`
+let arr = [...document.querySelectorAll('.square_1')].map(function(el) {
+    el.style.transform = `translate(${anime.random(0, 300)}vw, ${anime.random(0, 50)}vh)`
 })
+let numberOfWords = 18
+  for (let i = 0; i < numberOfWords; i++){
+    arr.push('.square_1')
+    return arr
+}
+let animatedMorph = anime({
+  targets: '.name img',
+  keyframes: [
+    {translateY: 10},
+    {translateX: -40},
+    {translateY: -40},
+    {translateX: 30},
+    {translateY: 20},
+    {translateX: 45},
+    {translateY: 10},
+    {translateX: -40},
+    {translateY: -40},
+    {translateX: 30},
+    {translateY: 20},
+    {translateX: 45}
+  ],
+  loop: true,
+  direction: 'alternate',
+  duration: 20000,
+  easing: 'easeInQuad'
+})
+
+// function duplicate(arr){
+//   let temp = arr.slice()
+//   for (i = 0; i < arr.length; i++){
+//     temp.push(arr[i])
+//   }
+//   return temp
+// }
