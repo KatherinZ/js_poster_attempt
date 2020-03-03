3607aa769dcc2ede49e1b5d868f6630ca1b02f32
Katherin_Z <zyrianovakate10@gmail.com>
2020-03-03 13:09:26 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -15,35 +15,33 @@
 // onload shuffle(elementsArray)
 
 let arr = [...document.querySelectorAll('.square_1')].map(function(el) {
-    el.style.transform = `translate(${anime.random(0, 300)}vw, ${anime.random(0, 50)}vh)`
+    el.style.transform = `translate(${anime.random(0, 300)}vw, ${anime.random(0, -30)}vh)`
 })
-let numberOfWords = 18
-  for (let i = 0; i < numberOfWords; i++){
-    arr.push('.square_1')
-    return arr
-}
+// let numberOfWords = 18
+//   for (let i = 0; i < numberOfWords; i++){
+//     arr.push('.square_1')
+//     return arr
+// }
 let animatedMorph = anime({
   targets: '.name img',
-  keyframes: [
-    {translateY: 10},
-    {translateX: -40},
-    {translateY: -40},
-    {translateX: 30},
-    {translateY: 20},
-    {translateX: 45},
-    {translateY: 10},
-    {translateX: -40},
-    {translateY: -40},
-    {translateX: 30},
-    {translateY: 20},
-    {translateX: 45}
+  scaleX: [
+    { value: 4, duration: 13000},
+    { value: 1, duration: 9200},
+    { value: 4, duration: 9400, delay: 500},
+    { value: 1, duration: 10300 }
   ],
+  scaleY: [
+  { value: [1.75, 1], duration: 13000},
+  { value: 2, duration: 9200 },
+  { value: 1, duration: 8900 },
+  { value: 1.75, duration: 9600, delay: 1000},
+  { value: 1, duration: 11000 }
+],
   loop: true,
   direction: 'alternate',
-  duration: 20000,
-  easing: 'easeInQuad'
+  easing: 'linear'
 })
-
+document.getElementsByClassName('all_elements').style.height = 1200 + 'px'
 // function duplicate(arr){
 //   let temp = arr.slice()
 //   for (i = 0; i < arr.length; i++){
